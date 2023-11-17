3e99b1f1b2c88681a0ef57f2b6d7f7f731703fdf
Daniel Ramirez <dani_0318@hotmail.com>
2023-11-17 17:15:00 -0500

    add validation in dynamic form and add add and delete favorite game in form

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,6 @@
 import { group } from '@angular/animations';
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -16,6 +16,8 @@ export class DynamicPageComponent {
       ['Death Stranding', Validators.required],
     ])
   });
+
+  public newFavorite: FormControl = new FormControl('', Validators.required);
 
   constructor(private fb: FormBuilder){}
 
@@ -56,9 +58,27 @@ export class DynamicPageComponent {
     }
 
     console.log(this.myForm.value);
-
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
     this.myForm.reset()
     
+  }
+
+  onDeleteFavorite(index:number):void{
+    this.favoriteGames.removeAt(index);
+  }
+
+  onAddToFavorites():void{
+    if(this.newFavorite.invalid) return;
+
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(
+      this.fb.control(newGame, Validators.required)
+    );
+
+    this.newFavorite.reset();
+    
+
   }
 
 }
