96918b67fbd4cce38ef95bb4d7339f10cd94431d
Daniel Ramirez <dani_0318@hotmail.com>
2023-11-18 17:57:25 -0500

    add  validation in register page and create service for validations

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: [
   ]
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit{
+
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.myForm.reset();
+  }
+
+  idValidField(field:string){
+    
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 
 }
+
