fa59994f1b615cad2b70345f568125fc630bc4d3
Daniel Ramirez <dani_0318@hotmail.com>
2023-11-25 11:27:34 -0500

    Add validator for register form and project finished

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from 'src/app/shared/validators/validators';
+// import * as customValidators from 'src/app/shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -11,21 +13,26 @@ export class RegisterPageComponent implements OnInit{
 
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required,Validators.pattern(customValidators.emailPattern)]],
-    username: ['', [Validators.required, customValidators.cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    // email: ['', [Validators.required,Validators.pattern(this.validatorsService.emailPattern)], [new EmailValidatorService]], //otra forma pero puede afectar el performance
+    email: ['', [Validators.required,Validators.pattern(this.validatorsService.emailPattern)], [this.emailValidatorService]],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
+  }, {
+    validators: [
+      this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2')
+    ]
   });
 
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder, private validatorsService:ValidatorsService, private emailValidatorService:EmailValidatorService ){}
 
   ngOnInit(): void {
     this.myForm.reset();
   }
 
   idValidField(field:string){
-    
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   onSubmit(){
